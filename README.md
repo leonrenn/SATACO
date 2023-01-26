b990fe359d0b1509dc3a69192c41259fc63cb9fc
leonrenn <leon.ransf@gmail.com>
2023-01-26 15:38:21 +0100

    Added visualization and updated readme.

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # SATACO
 On top tool for Simple Analysis (SA) implementing algorithms from Testing Analyses' COrrelations (TACO).
+
+## CLI
+
+The tool is meant to be a CL tool. _SimpleAnalysis_ should be installed,<br>
+to do real world applications. For testing and newcomers just provide the <br>
+command 
+```sh
+python src/main.py -r data/Diphoton2016.root,data/DMbb2016.root
+```
+
--- src/main.py
+++ src/main.py
@@ -163,6 +163,19 @@ def main() -> int:
     df_SR_SR.to_csv(str(pathlib.Path(__file__).parent.resolve()) +
                     "/../results/SR_SR.csv", index=False, header=True)
 
+    # 4) VISUALIZATION
+    fig, ax = plt.subplots()
+    ax.pcolor(SR_SR_matrix, cmap=plt.cm.Blues)
+    # put the major ticks at the middle of each cell
+    ax.set_xticks(np.arange(len(column_names)) + 0.5, minor=False)
+    ax.set_yticks(np.arange(len(column_names)) + 0.5, minor=False)
+    # want a more natural, table-like display
+    ax.invert_yaxis()
+    ax.xaxis.tick_top()
+    ax.xaxis.set_ticks_position('both')
+    ax.set_xticklabels(column_names, minor=False)
+    ax.set_yticklabels(column_names, minor=False)
+    plt.show()
     return 0
 
 
