c7ee275ddc41c14e579320ecc110dae137c24882
leonrenn <leon.ransf@gmail.com>
2023-01-26 11:56:38 +0100

    Reading multiple files into numpy matrix format.

--- src/exceptions/exceptions.py
+++ src/exceptions/exceptions.py
@@ -2,3 +2,17 @@ class NonSimpleAnalysisFormat(Exception):
     """Raised when the input file is not of the
     output of the SimpleAnalysis tool."""
     pass
+
+
+class SAFileNotFoundError(FileNotFoundError):
+    """Raised when the files are not found 
+    at the beginning of the SATACO programm.
+    """
+    pass
+
+
+class SAValueError(ValueError):
+    """Raised when files cannot be read because
+    they are in the wrong format.
+    """
+    pass
--- src/main.py
+++ src/main.py
@@ -1,12 +1,16 @@
 import os
+import re
 from argparse import ArgumentParser
 from typing import Dict, List
 
+import matplotlib.pyplot as plt
+import numpy as np
 import uproot as ur
 from tqdm import tqdm
 from uproot.reading import ReadOnlyFile
 
-from exceptions.exceptions import NonSimpleAnalysisFormat
+from exceptions.exceptions import (NonSimpleAnalysisFormat,
+                                   SAFileNotFoundError, SAValueError)
 from utils.info import info
 
 # MAIN
@@ -28,6 +32,7 @@ def main() -> int:
     parser_dict: Dict[str, str] = vars(parser.parse_args())
     # split files in csv format from CLI
     file_paths: List[str] = parser_dict["root"].split(",")
+    analysis_names: List[str] = []
 
     # 2) CHECK FOR EXISTENCE & FOR SA-FORMAT (BEFORE SCAN)
     # the specific output format is specified under
@@ -36,28 +41,59 @@ def main() -> int:
     print("Checking correctness of input:\n")
     try:
         for file_path in tqdm(file_paths):
+            analysis_name: str = os.path.basename(file_path).strip(".root")
+            analysis_names.append(analysis_name)
+
             if os.path.exists(file_path) is False:
-                raise FileNotFoundError
+                raise SAFileNotFoundError
             temp: ReadOnlyFile = ur.open(file_path)
-            classnames: str = temp.classnames()
+            classnames = temp.classnames()
             temp.close()
-            if str(classnames) != "{'ntuple;1': 'TTree'}":
+            if (str(classnames) != "{'ntuple;1': 'TTree'}" and
+                    re.search(".*'ntuple;1'.*", str(classnames)) is not None):
                 raise NonSimpleAnalysisFormat
-    except FileNotFoundError:
+    except SAFileNotFoundError:
         print(f"The file {file_path} could not be found. Exit.")
         return 1
 
-    except ValueError:
-        print(f"The file {file_path} could not be read "
+    except SAValueError:
+        print(f"The file {file_path} could not be read \n"
               "with the uproot python module. Exit.")
         return 2
 
     except NonSimpleAnalysisFormat:
-        print(f"The file {file_path} is in the format of "
-              f"{str(temp.classnames())}. This is not in the"
-              " expected SA format of {'ntuple;1': 'TTree'}."
+        print(f"The file {file_path} is in the format of \n"
+              f"{str(temp.classnames())}. This is not in the\n"
+              "expected SA format [-n] of {'ntuple;1': 'TTree'}.\n"
+              "Do not use option '-o' in simpleAnalysis.\n"
               "Exit.")
         return 3
+
+    # 3) RUN ANALYSIS ON INPUT
+    # info about which analyses where provided to SATACO
+    print("\nAnalyses SATACO runs on:")
+    for analysis_name in analysis_names:
+        print(f"- {analysis_name}")
+    print("\n")
+
+    # 3.1) For analyses that are not combined
+    print("Files preprocessing:\n")
+    for file_idx, file_path in enumerate(tqdm(file_paths)):
+        # opening the file
+        with ur.open(file_path) as file:
+            # access to the ntuple structure
+            ttree = file["ntuple"]
+            # signal regions are the keys of the ttree
+            signal_regions = ttree.keys()
+            # signal regions with counts of events
+            # that passed
+            ttree_arrays = ttree.arrays()
+            # empty matrix to store data in numpy style
+            events = np.empty((len(ttree_arrays), len(signal_regions)))
+            # iterating through signal regions to extract the
+            # arrays and store row wise
+            for sr_idx, sr in enumerate(signal_regions):
+                events[:, sr_idx] = ttree_arrays[sr]
 
     return 0
 
